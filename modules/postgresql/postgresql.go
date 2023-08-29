51b2bf76454a092bd755a0756e06327d5a675f83
Eugene (korsilyn) Gladkov <korsilyn@pm.me>
2023-08-29 11:08:13 +0300

    HTTP and PostgreSQL basic modules

---
+++ modules/postgresql/postgresql.go
@@ -0,0 +1,81 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"time"
+)
+
+const (
+	defaultMaxPoolSize = 1
+	defaultConnAttempts = 10
+	defaultConnTimeout = time.Second
+)
+
+type PgxPool interface {
+	Close()
+	Acquire(ctx context.Context) (*pgxpool.Conn, error)
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
+	Ping(ctx context.Context) error
+}
+
+type PostgreSQL struct {
+	maxPoolSize int
+	connAttempts int
+	connTimeout time.Duration
+	
+	Builder squirrel.StatementBuilderType
+	Pool PgxPool
+}
+
+func New(url string) (*PostgreSQL, error) {
+	pg := &PostgreSQL{
+		maxPoolSize: defaultMaxPoolSize,
+		connAttempts: defaultConnAttempts,
+		connTimeout: defaultConnTimeout,
+	}
+
+	pg.Builder = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+	poolConfig, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, fmt.Errorf("PostgreSQL - New - pgxpool.ParseConfig: %w", err)
+	}
+
+	poolConfig.MaxConns = int32(pg.maxPoolSize)
+
+	for pg.connAttempts > 0 {
+		pg.Pool, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
+		if err == nil {
+			break
+		}
+
+		log.Printf("PostgreSQL is trying to connect, attempts left: %d", pg.connAttempts)
+		time.Sleep(pg.connTimeout)
+		pg.connAttempts--
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("PostgreSQL - New - pgxpool.Connect: %w", err)
+	}
+
+	return pg, nil
+}
+
+func (p *PostgreSQL) Close() {
+	if p.Pool != nil {
+		p.Pool.Close()
+	}
+}
+
